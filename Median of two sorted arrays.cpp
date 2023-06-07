1c74fb2ce9046a9c21471a533134103184a69e5a
shilpy chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-07 08:17:12 +0530

    Update Median of two sorted arrays.cpp

--- Median of two sorted arrays.cpp
+++ Median of two sorted arrays.cpp
@@ -1,1 +1,33 @@
-
+class Solution {
+public:
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+       int m = nums1.size();
+       int n = nums2.size();
+       vector<int> merged;
+        int i = 0, j = 0; // pointers for nums1 and nums2
+        while (i < m && j < n) { // while both arrays have values remaining
+        if (nums1[i] < nums2[j]) {
+        merged.push_back(nums1[i]);
+        i++;
+        } else {
+        merged.push_back(nums2[j]);
+        j++;
+        }
+}
+// add any remaining values from nums1 or nums2
+        while (i < m) {
+        merged.push_back(nums1[i]);
+        i++;
+        }
+        while (j < n) {
+        merged.push_back(nums2[j]);
+        j++;
+        }
+        int size = merged.size();
+        if (size % 2 == 1) { // if size is odd 
+        return merged[size / 2];
+        } else { // if size is even
+        return (merged[size / 2] + merged[size / 2 - 1]) / 2.0;
+        }
+        }
+};
