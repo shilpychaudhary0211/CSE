04832aa097d953515a0535072cd68d663ef72cef
Shilpy Chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-10 20:46:42 +0530

    Update valid parenthesis.cpp

--- valid parenthesis.cpp
+++ valid parenthesis.cpp
@@ -1,1 +1,18 @@
+bool validparenthesis(string s){
 
+    stack<char>stk;
+
+    for(auto i : s){
+        if(i=='(' || i=='{' || i=='['){
+            stk.push(i);
+        }
+        else if(!stk.empty() && (stk.top()=='(' && i==')') || (stk.top()=='[' && i==']') || (stk.top()='{' && i=='}')){
+            stk.pop();
+        }
+        else{
+            return false;
+        }
+    }
+    return stk.empty();
+
+}
