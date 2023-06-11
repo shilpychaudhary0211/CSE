6d04948f15df8dc3cdc505bd876325533c59f14b
Shilpy Chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-11 19:12:33 +0530

    Update three sum.cpp

--- three sum.cpp
+++ three sum.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> res;
+    int n = nums.size();
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (i != j && i != k && j != k && nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> triplet = {nums[i], nums[j], nums[k]};
+                    sort(triplet.begin(), triplet.end());
+                    if (find(res.begin(), res.end(), triplet) == res.end()) {
+                        res.push_back(triplet);
+                    }
+                }
+            }
+        }
+    }
+    return res;
+}
+};
