618fd5999c1a143f25473ed71125df9a4499dfe3
Shilpy Chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-11 11:38:07 +0530

    Update container with water.cpp

--- container with water.cpp
+++ container with water.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxArea=0;
+
+        int l=0;
+        int r = height.size() -1;
+
+        while(l<r){
+            int lh = height[l];
+            int rh = height[r];
+
+            int min_h = min(lh,rh);
+
+            maxArea = max(maxArea, min_h*(r-l));
+
+            if(lh<rh){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return maxArea;
+   
+    } 
+    
+};
 
