d5676292389a1788cd1bfe3df12c73fbe1596c19
Shilpy Chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-06-30 10:50:07 +0530

    Update zigzag conversion.cpp

--- zigzag conversion.cpp
+++ zigzag conversion.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    string convert(string s, int n) {
 
+        if(n==1)
+        return s;
+        vector<string>ans(n);
+        int flag=1;
+        int j=0;
+        for(auto it:s){
+            if(flag==1){
+                ans[j]+=it;
+                j++;
+                if(j==n){
+                    j-=2;
+                    flag=2;
+                }
+            }
+            else{
+                ans[j]+=it;
+                j--;
+                if(j==-1){
+                    j+=2;
+                    flag=1;
+                }
+            }
+        }
+        string finalans;
+        for(auto str:ans)
+            finalans+=str;
+        return finalans;
+
+        
+    }
+};
