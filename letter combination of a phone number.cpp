eaa6c23645f8611a1e6b95d6bd502ab63bd77903
Shilpy Chaudhary <133845445+shilpychaudhary0211@users.noreply.github.com>
2023-07-24 19:51:47 +0530

    Update letter combination of a phone number.cpp

--- letter combination of a phone number.cpp
+++ letter combination of a phone number.cpp
@@ -1,1 +1,35 @@
+class Solution {
 
+    private:
+void solve(string digits,string mapping[],int index,string out,vector<string>&ans)
+{
+    if(index>=digits.length())
+    {
+        ans.push_back(out);
+        return;
+    }
+    int num=digits[index]-'0';
+    string val=mapping[num];
+    for(int i=0;i<val.length();i++)
+    {
+        out.push_back(val[i]);
+        solve(digits,mapping,index+1,out,ans);
+        out.pop_back();
+    }
+}
+
+
+public:
+    vector<string> letterCombinations(string digits) {
+
+        vector<string>ans;
+        int index=0;
+        string out;
+        if(digits=="")
+        return ans;
+        string mapping[10]={" "," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,mapping,index,out,ans);
+        return ans;
+        
+    }
+};
